caa6fb543a2dc8a91f58a99f5a21d8367fd378f9
harshgupta9321 <112786943+harshgupta9321@users.noreply.github.com>
2024-09-03 14:36:42 +0530

    change host

--- chat-app/src/utils/APIRoutes.js
+++ chat-app/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = "https://chat-app-backend-7c1t.onrender.com";
 
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
